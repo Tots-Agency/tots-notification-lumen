998619ed1fb8a725b7d3dac0080e8d58b130f11f
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-18 20:15:53 -0300

    Add list me controller.

---
+++ src/Http/Controllers/ListMeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tots\Notification\Http\Controllers\Posts;
+
+use Illuminate\Http\Request;
+use Tots\Notification\Models\TotsNotification;
+
+class ListMeController extends \Laravel\Lumen\Routing\Controller
+{
+    public function handle(Request $request)
+    {
+        // Create query
+        $elofilter = \Tots\EloFilter\Query::run(TotsNotification::class, $request);
+        // Custom filters
+        $elofilter->getQueryRequest()->addWhere('user_id', $request->user()->id);
+        // Execute query
+        return $elofilter->execute();
+    }
+}
